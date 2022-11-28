a03a8a50027f9081316b8836733f948614b1d3ff
PiotrO9 <piotr.ostrowski02@wp.pl>
2022-11-28 06:31:38 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+import { renderDashboard } from "./view-dashboard.js";
+import { renderDetail } from "./view-detail.js";
+
+const returnOriginalStyles = () => {
+    const mainDashBoard = document.getElementById("main__dashboard");
+    mainDashBoard.style.display = "grid";
+
+    const mainContent = document.getElementById("main__content");
+    mainContent.style.display = "flex";
+    mainContent.style.flexDirection = "column";
+};
+
+if(window.location.search.includes("?country=")) {
+    document.getElementById("main__content__filters").style.display = "none";
+    renderDetail();
+}
+else {
+    returnOriginalStyles();
+    renderDashboard();
+}
+
